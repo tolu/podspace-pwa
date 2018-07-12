3e75f37475e28a82c0a03b87ab1cbc3d8504d659
Tobias Lundin <tobias.lundin@gmail.com>
2018-07-13 00:59:57 +0200

    add fallback for rss feeds to handle NO-CORS feeds seems we loose all itunes-ns properties but maybee we can fix that

--- src/feedService.ts
+++ src/feedService.ts
@@ -1,5 +1,6 @@
 import { createCache } from "./cache";
 import { rssStringToJson, RssItems } from './rssStringToJson';
+import { durationSecToString } from "./utils";
 
 const cache = createCache('pod-feed');
 
@@ -10,13 +11,68 @@ export const getFeedItems = async (feedUrl: string): Promise<RssItems> => {
     return cacheResponse;
   }
   // get from network and add to cache
-  const response = await fetch(feedUrl);
-  const text = await response.text();
+  let response: Response;
+  let text: string = '';
+  let json: any;
+  try {
+    response = await fetch(feedUrl);
+    text  = await response.text();
+  } catch (error) {
+    console.error('OH NOES', error);
+    console.warn('trying fallback via https://api.rss2json.com/v1/api.json ...');
+    response = await fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(feedUrl)}`);
+    json = await response.json();
+  }
+  
   if (response.ok) {
-    const data = rssStringToJson(text);
+    const data = json ? toRssItems(json) : rssStringToJson(text);
     cache.set(feedUrl, data);
     return data;
   }
   console.warn('Request failed', response);
   return { items: [] };
+}
+
+function toRssItems(rssData: RssApiResponse): RssItems {
+  const total = rssData.items.length;
+  return {
+    items: rssData.items.map((i, idx) => ({
+      title: i.title,
+      duration: durationSecToString(i.enclosure.duration),
+      enclosure: {
+        url: i.enclosure.link,
+        length: i.enclosure.duration,
+        type: i.enclosure.type,
+      },
+      episode: (total - idx).toString(),
+      image: {
+        href: i.thumbnail
+      },
+      pubDate: i.pubDate,
+      subtitle: i.content,
+      summary: i.description,
+    }))
+  }
+}
+
+interface RssApiResponse {
+  items: RssApiItem[]
+}
+interface RssApiItem {
+  title: string;
+  pubDate: string;
+  link: string;
+  guid: string;
+  author: string;
+  thumbnail: string;
+  description: string;
+  content: string;
+  enclosure: Enclosure;
+}
+
+interface Enclosure {
+  link: string;
+  type: string;
+  duration: number;
+  thumbnail: string;
 }
